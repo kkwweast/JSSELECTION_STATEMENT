0222236b0558a25e0867bc7307f24d7caa38b9df
Kim Weast <weastkim3@gmail.com>
2023-10-13 16:45:45 -0500

    update

---
+++ app.js
@@ -0,0 +1,56 @@
+//Exercise 1: Guessing game 
+if (isSecureContext(guessNumber) || guessNumber)
+
+let magicNumber = 3;
+
+//Get the users guess
+//Return a string
+  } else  {  
+let guessNumber = prompt("What is the magic number?");
+if (guessNumber == magicNumber) {
+    console.log("Congrats! You guessed the magic number.");
+    } else if ( guessNUmber > magicNumber)  {
+        console.log("Guess was too high :/. Try again.");
+         } else { 
+            console.log("Guess was too low >:(. Try again.);
+    }
+
+
+    };
+//covert the
+
+
+
+    Let birthMonth = prompt("Please insert your birth month:" );
+
+    switch (birthMoth){ 
+       case "December":
+       case "January":
+       case "February":
+       console.log("winter") ;
+       break;
+       case "March":
+       case "April":
+       case "May":
+        console.log("Spring");
+       case "June":
+       case "July":
+       case "August":
+        console.log("Summer");
+        break;
+        case "September":
+        case "October":
+        case "November":
+            vonsole.log("Fall");
+            break;
+            default:
+            console.log("I don't reconize that month...");
+     
+        
+        break;
+    case "June", "July", "August":
+        console.log("Summer")
+    }   break;
+    case 
+     default :
+        console.log("I don't reconize that month...");
